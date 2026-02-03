9c46e8f2ef20bebef1decd70592f5c2974ce6e7d
ryna-h <ryna_99@protonmail.com>
2026-02-02 20:42:03 -0600

    work on adding paddle

--- tpong.c
+++ tpong.c
@@ -7,28 +7,33 @@
 
 int main(int argc, char *argv[]) {
 
-	int x=0, y=0;
-	int maxX=0, maxY=0;
-	int nextX=0;
-	int direction=1;
+	int xBall=0, yBall=0; // serve as active coordinates for ball
+	int yLbar=0;
+	int xMax=0, yMax=0; //max x & y coords. may need to reinitialize these in case user changes screen size.
+	int nextX=0, nextY=0; // should probably set these to middle of screen. 
+	int direction=1; //sets direction of ball if positive right negative left.
     
 	initscr(); //sets up screen 
  	noecho(); //tells terminal to not echo keypresses
 	curs_set(FALSE); //hides cursor in window
+	
+	getmaxyx(stdscr, yMax, xMax); //gets screen size in xy coords. stdscr is initialized by initscr()
+	xBall = xMax/2;
+	yBall = yMax/2;
 
 	while(1){
-		getmaxyx(stdscr, maxY, maxX); //gets screen size in xy coords. stdscr is initialized by initscr()
 		clear(); //clears screen
-		mvprintw(y,x,"o"); //prints in y,x coordinates
+		mvprintw(yBall,xBall,"o"); //prints in y,x coordinates
+		mvprintw(yLbar,0,"|");
 		refresh(); //refreshes screen
 		usleep(DELAY);
-		nextX = x + direction;
+		nextX = xBall + direction;
 
-		if (nextX >= maxX || nextX < 0){
-			direction*= -1;
+		if (nextX >= xMax || nextX < 0){ //for loop that checks wall limits and swaps ball direction
+			direction = direction * -1;
 		} 
 		else{
-			x+= direction;
+			xBall+= direction;
 		}
 	}
 	endwin(); //after program is finished returns to normal terminal
