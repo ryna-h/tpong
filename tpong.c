66d8930e227ed630d227b9b7b825aeb1133dd6e6
ryna-h <ryna_99@protonmail.com>
2026-02-08 20:41:55 -0600

    add left window setup

--- tpong.c
+++ tpong.c
@@ -1,15 +1,43 @@
-#include<stdio.h>
 #include<unistd.h>
-#include<stdlib.h>
 #include<ncurses.h>
+#include<locale.h>
 
 #define DELAY 30000
 
+typedef struct{ //sets up struct for window borders.
+	chtype 	ls, rs, ts, bs, 
+	 	tl, tr, bl, br;
+}WIN_BORDER;
+
+typedef struct{ //sets up sizing & position for box
+	int startx, starty;
+	int height, width;
+	WIN_BORDER border;
+}WIN;
+
+typedef struct{
+	int ballX, ballY;
+	char ballChar;
+}BALL;
+
+void init_win_params(WIN *p_win);
+void create_box(WIN *win, bool flag);
+
 int main(int argc, char *argv[]) {
 
+	setlocale(LC_ALL, "");	
+	//initialize needed objects
+	WIN lWin;
+	WIN rWin;
+	BALL ball;
+
 	int xBall=0, yBall=0; // serve as active coordinates for ball
-	int yLbar=0;
 	int xMax=0, yMax=0; //max x & y coords. may need to reinitialize these in case user changes screen size.
+	getmaxyx(stdscr, yMax, xMax); //gets screen size in xy coords. stdscr is initialized by initscr()
+	int ch;
+	xBall = xMax/2;
+	yBall = yMax/2;
+
 	int nextX=0, nextY=0; // should probably set these to middle of screen. 
 	int direction=1; //sets direction of ball if positive right negative left.
     
@@ -17,14 +45,18 @@ int main(int argc, char *argv[]) {
  	noecho(); //tells terminal to not echo keypresses
 	curs_set(FALSE); //hides cursor in window
 	
-	getmaxyx(stdscr, yMax, xMax); //gets screen size in xy coords. stdscr is initialized by initscr()
-	xBall = xMax/2;
-	yBall = yMax/2;
-
+	while((ch = getch()) != KEY_F(1)){
+		switch(ch){
+			case 'w':
+				create_box(&lWin, FALSE);
+				++lWin.starty;
+				create_box(&lWin, TRUE);
+		}
+	}
+	
 	while(1){
 		clear(); //clears screen
 		mvprintw(yBall,xBall,"o"); //prints in y,x coordinates
-		mvprintw(yLbar,0,"|");
 		refresh(); //refreshes screen
 		usleep(DELAY);
 		nextX = xBall + direction;
@@ -39,37 +71,44 @@ int main(int argc, char *argv[]) {
 	endwin(); //after program is finished returns to normal terminal
 }
 
-
-WINDOW *create_newwin(int height, int width, int starty, int startx) //create window function. pulled from wiki
-{	WINDOW *local_win;
-
-	local_win = newwin(height, width, starty, startx);
-	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
-					 * for the vertical and horizontal
-					 * lines			*/
-	wrefresh(local_win);		/* Show that box 		*/
-
-	return local_win;
+// window parameter setup
+void init_win_params(WIN *p_win){
+	p_win->height = 5;
+	p_win->width = 2;
+	p_win->starty = (LINES - p_win->height)/2; //need to look into this	
+	p_win->startx = (COLS - p_win->width)/2; // need to look into this
+	p_win->border.ls = ACS_VLINE;
+	p_win->border.rs = ACS_VLINE;
+	p_win->border.ts = ACS_HLINE;
+	p_win->border.bs = ACS_HLINE;
+	p_win->border.tl = ACS_ULCORNER;
+	p_win->border.tr = ACS_URCORNER;
+	p_win->border.bl = ACS_LLCORNER;
+	p_win->border.br = ACS_LRCORNER;
 }
+void create_box(WIN *p_win, bool flag){
+	int i, j;
+	int x, y, w, h;
 
-void destroy_win(WINDOW *local_win) //function that erases box so it can be "moved"
-{	
-	/* box(local_win, ' ', ' '); : This won't produce the desired
-	 * result of erasing the window. It will leave it's four corners 
-	 * and so an ugly remnant of window. 
-	 */
-	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	/* The parameters taken are 
-	 * 1. win: the window on which to operate
-	 * 2. ls: character to be used for the left side of the window 
-	 * 3. rs: character to be used for the right side of the window 
-	 * 4. ts: character to be used for the top side of the window 
-	 * 5. bs: character to be used for the bottom side of the window 
-	 * 6. tl: character to be used for the top left corner of the window 
-	 * 7. tr: character to be used for the top right corner of the window 
-	 * 8. bl: character to be used for the bottom left corner of the window 
-	 * 9. br: character to be used for the bottom right corner of the window
-	 */
-	wrefresh(local_win);
-	delwin(local_win);
+	x = p_win->startx;
+	y = p_win->starty;
+	w = p_win->width;
+	h = p_win->height;
+
+	if(flag == TRUE)
+	{	mvaddch(y, x, p_win->border.tl);
+		mvaddch(y, x + w, p_win->border.tr);
+		mvaddch(y + h, x, p_win->border.bl);
+		mvaddch(y + h, x + w, p_win->border.br);
+		mvhline(y, x + 1, p_win->border.ts, w - 1);
+		mvhline(y + h, x + 1, p_win->border.bs, w - 1);
+		mvvline(y + 1, x, p_win->border.ls, h - 1);
+		mvvline(y + 1, x + w, p_win->border.rs, h - 1);
+
+	}
+	else
+		for(j = y; j <= y + h; ++j)
+			for(i = x; i <= x + w; ++i)
+				mvaddch(j, i, ' ');
+	refresh();
 }
